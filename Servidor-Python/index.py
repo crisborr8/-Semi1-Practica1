e5c44f36010cb9291dd55a1d1996d3bdfecdd2af
milton <3018849780101@ingenieria.usac.edu.gt>
2022-02-24 00:38:40 -0600

    User module
    Create user
    View all users
    Login
    User photo
    Edit user
    Save photos in S3 bucket

---
+++ Servidor-Python/index.py
@@ -0,0 +1,40 @@
+from crypt import methods
+import os
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+import boto3
+
+client = boto3.client(
+    's3',
+    region_name = os.environ['REGION'],
+    aws_access_key_id = os.environ['KEY'],
+    aws_secret_access_key = os.environ['SECRET_KEY']
+)
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
+app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    var = os.environ['asdf']
+    print(var)
+    return ('Python api is running')
+
+import user
+
+if __name__ == '__main__':
+    app.run(port = 3000, host='0.0.0.0',debug=True)
+
+"""
+from flask import jsonify, make_response
+@app.route('/summary')
+def summary():
+    d = make_summary()
+    return make_response(jsonify(d), 200)
+"""
---
+++ Servidor-Python/user.py
@@ -0,0 +1,105 @@
+from __main__ import app, mysql, client
+from fileinput import filename
+from flask import jsonify, request
+import base64
+import os
+
+@app.route('/newUser', methods=['POST'])
+def newUser():
+    #data from request
+    data = request.get_json()
+    username = data.get('username')
+    name = data.get('name')
+    contra = data.get('contra')
+    foto = data.get('foto')
+    #decode buffer from base64
+    foto64 = base64.b64decode(foto)
+    #query
+    sql = "INSERT INTO usuario (username,name,contra) VALUES ('{}','{}','{}')".format(username,name,contra)
+    #execute query
+    cur = mysql.connection.cursor()
+    try:#create user
+        cur.execute(sql)
+        mysql.connection.commit()
+        cur.close()
+        client.put_object(
+            Body = foto64,
+            Bucket = os.environ['BUCKET'],
+            Key = 'foto-perfil/{}.jpg'.format(username),
+            ContentType = 'image'
+        )
+        return "user added: {}".format(username)
+    except:#error creating user
+        cur.close()
+        return "Error adding user"
+
+@app.route('/allUsers')
+def allUsers():
+    sql = "SELECT * FROM usuario"
+    cur = mysql.connection.cursor()
+    cur.execute(sql)
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
+
+@app.route('/login', methods=['POST'])
+def oneUser():
+    #data from request
+    data = request.get_json()
+    username = data.get('username')
+    contra = data.get('contra')
+    #query
+    sql = "SELECT * FROM usuario WHERE username = '{}' AND contra = '{}'".format(username,contra)
+    #execute query
+    cur = mysql.connection.cursor()
+    cur.execute(sql)
+    data = cur.fetchall()
+    cur.close()
+    if len(data) > 0:#user found
+        return jsonify(data[0])
+    else:
+        #no user found
+        return jsonify(data)
+
+@app.route('/userPhoto', methods=['POST'])
+def userPhoto():
+    #data from request
+    data = request.get_json()
+    username = data.get('username')
+    obj = client.get_object(
+        Bucket = os.environ['BUCKET'],
+        Key = 'foto-perfil/{}.jpg'.format(username)
+    )
+    #bytes buffer
+    foto = obj.get('Body').read()
+    #decode buffer
+    foto = base64.b64encode(foto).decode('utf-8')
+    return jsonify(foto)
+
+@app.route('/editUser', methods=['PATCH'])
+def editUser():
+    #data from request
+    data = request.get_json()
+    id = data.get('idusuario')
+    username = data.get('username')
+    name = data.get('name')
+    foto = data.get('foto')
+    #decode buffer from base64
+    foto64 = base64.b64decode(foto)
+    #query
+    sql = "UPDATE usuario SET username = '{}', name = '{}' WHERE idusuario = {}".format(username,name,id)
+    #execute query
+    cur = mysql.connection.cursor()
+    try:#update user
+        cur.execute(sql)
+        mysql.connection.commit()
+        cur.close()
+        client.put_object(
+            Body = foto64,
+            Bucket = os.environ['BUCKET'],
+            Key = 'foto-perfil/{}.jpg'.format(username),
+            ContentType = 'image'
+        )
+        return "user {} updated".format(id)
+    except:#error trying to update
+        return "Error trying to update user"
